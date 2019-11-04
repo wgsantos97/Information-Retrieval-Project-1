18a7271a638028cdda49796aae507e3602b8afe1
William Santos <wsantos1@pride.hofstra.edu>
2019-11-03 22:49:17 -0500

    Part 2 started. Testing required.

--- Data/RawData/processRaws.py
+++ Data/RawData/processRaws.py
@@ -1,16 +1,20 @@
 import json
 
-# This only ever needs to run once. To process the 
+# This only ever needs to run once.
+
+
 def main():
     startfile = "graphic_novels_raw.json"
     endfile = "../graphic_novels.json"
     f = open(startfile, 'r')
     data = dict()
     for line in f:
+        #entry = dict()
         d = json.loads(line)
         key = d["book_id"]
         value = d["title"]
         data[key] = value
+        #data.append(entry)
     f.close()
 
     f = open(endfile, 'w')
